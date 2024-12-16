d8cb3e4bca07201ed2f5927fc8d0ea92a0664707
R. Fitzgerald <AustralianSoftwareEngineer@gmail.com>
2024-12-17 05:33:19 +1100

    Slowed down the spam. Fixed temperature conversion.

--- main/main.ino
+++ main/main.ino
@@ -1,8 +1,14 @@
+unsigned long Time = 0;
+
 void setup() {
   Serial.begin(9600);
+  Time = millis();
 }
 
 void loop() {
-  Serial.print("Temperature: ");
-  Serial.println((analogRead(A0) / 1.8));
+  if ((millis() - Time) > 1000) {
+    Serial.print("Temperature: ");
+    Serial.println((analogRead(A0) * 0.488));
+    Time = millis();
+  }
 }
