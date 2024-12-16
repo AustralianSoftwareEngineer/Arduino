206274e727f704138df89fd435de558552de9d8e
R. Fitzgerald <AustralianSoftwareEngineer@gmail.com>
2024-12-17 05:44:21 +1100

    Significantly improved input handler and readability.

--- main/main.ino
+++ main/main.ino
@@ -1,43 +1,46 @@
-int pin = 0;
-int val = 0;
-int dOn[7] = {0,0,0,0,0,0,0};
 unsigned long Time = 0;
+
+int selectedLedPin = 0;
+int LedDisplayArray[7] = {0,0,0,0,0,0,0};
+
+bool logTemperature = false;
 
 void setup() {
   Serial.begin(9600);
   Time = millis();
+
   SegmentStartUp();
-  
   TEST_7SEGMENT_LED_TEST(); //Used for testing 7 segment array.
   // put your setup code here, to run once:
 }
 
 void loop() {
-  //TemperatureOutput();
-  InputManagerLED();
-  Display();
+  if (logTemperature) {
+    TemperatureLogger();
+  }
+  InputManager();
+}
+
+void Display(){
+  digitalWrite(2, LedDisplayArray[0]);
+  digitalWrite(3, LedDisplayArray[1]);
+  digitalWrite(4, LedDisplayArray[2]);
+  digitalWrite(5, LedDisplayArray[3]);
+  digitalWrite(6, LedDisplayArray[4]);
+  digitalWrite(7, LedDisplayArray[5]);
+  digitalWrite(8, LedDisplayArray[6]);
 }
 
 void Display_A() {
-    dOn[0] = 0;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 1;
-    dOn[4] = 0;
-    dOn[5] = 0;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 1;
+    LedDisplayArray[4] = 0;
+    LedDisplayArray[5] = 0;
+    LedDisplayArray[6] = 0;
     Serial.println("Displaying the Letter A.");
     TEST_OUTPUT_DISPLAY_ARRAY();
-}
-
-void Display() {
-  digitalWrite(2, dOn[0]);
-  digitalWrite(3, dOn[1]);
-  digitalWrite(4, dOn[2]);
-  digitalWrite(5, dOn[3]);
-  digitalWrite(6, dOn[4]);
-  digitalWrite(7, dOn[5]);
-  digitalWrite(8, dOn[6]);
 }
 
 void TemperatureLogger() {
@@ -48,38 +51,67 @@ void TemperatureLogger() {
   }
 }
 
-//User inputs 2 numbers E.g.(11) Which means digitalWrite(3, HIGH).
-void InputManagerLED() {
-  if (Serial.available() > 0) {
-    // Check to see if pin is populated
-    if (pin) {
-      //Allocate the state
-      val = Serial.read()-48;
-      if (val > -1) {
-        dOn[pin-2] = val;
-        Serial.print("Updated pin: "); Serial.print(pin); Serial.print(". To state: "); Serial.println(val);
-        pin = 0;
-        val = 0;
-      }
+void InputManager() {
+    if (Serial.available() > 0) {
+        switch (Serial.read()) {
+            case 10:
+                break;
+            case 48:
+                LedDisplayArray[selectedLedPin] = 0;
+                break;
+            case 49:
+                LedDisplayArray[selectedLedPin] = 1;
+                break;
+            case 50:
+                selectedLedPin = 2;
+                break;
+            case 51:
+                selectedLedPin = 3;
+                break;
+            case 52:
+                selectedLedPin = 4;
+                break;
+            case 53:
+                selectedLedPin = 5;
+                break;
+            case 54:
+                selectedLedPin = 6;
+                break;
+            case 55:
+                selectedLedPin = 7;
+                break;
+            case 56:
+                selectedLedPin = 8;
+                break;
+            case 57:
+                selectedLedPin = 9;
+                break;
+            case 65:
+                Display_A();
+                break;
+            case 68:
+                TEST_ALL_OFF_TEST();
+                break;
+            case 84:
+                logTemperature = !logTemperature;
+                break;
+            case 97:
+                Display_A();
+                break;
+            case 99:
+                TEST_ALL_OFF_TEST();
+                break;
+            case 100:
+                TEST_ALL_OFF_TEST();
+                break;
+            case 116:
+                TemperatureLogger();
+                break;
+            default:
+                break;
+        }
     }
-    //Allocate the pin
-    else if (!pin) {
-      pin = Serial.read() -48;
-      if (pin == 9) {
-        TEST_OUTPUT_DISPLAY_ARRAY();
-        pin = 0;
-      }
-      if (pin == 1) {
-        Display();
-        pin = 0;
-      }
-      else if (pin < 0) {
-        pin = 0;
-      }
-    }
-  }
 }
-
 
 void SegmentStartUp() {
   pinMode(2, OUTPUT);
@@ -130,161 +162,168 @@ void TEST_7SEGMENT_LED_TEST() {
 
 void TEST_OUTPUT_DISPLAY_ARRAY() {
     Serial.print("2 is ");
-    Serial.println(dOn[0]);
+    Serial.println(LedDisplayArray[0]);
     Serial.print("3 is ");
-    Serial.println(dOn[1]);
+    Serial.println(LedDisplayArray[1]);
     Serial.print("4 is ");
-    Serial.println(dOn[2]);
+    Serial.println(LedDisplayArray[2]);
     Serial.print("5 is ");
-    Serial.println(dOn[3]);
+    Serial.println(LedDisplayArray[3]);
     Serial.print("6 is ");
-    Serial.println(dOn[4]);
+    Serial.println(LedDisplayArray[4]);
     Serial.print("7 is ");
-    Serial.println(dOn[5]);
+    Serial.println(LedDisplayArray[5]);
     Serial.print("8 is ");
-    Serial.println(dOn[6]);
+    Serial.println(LedDisplayArray[6]);
 }
 
 void TEST_Display_ZERO_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 0;
-    dOn[4] = 0;
-    dOn[5] = 0;
-    dOn[6] = 1;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 0;
+    LedDisplayArray[4] = 0;
+    LedDisplayArray[5] = 0;
+    LedDisplayArray[6] = 1;
     Serial.println("Testing the number ZERO : 0");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_ONE_TEST() {
-    dOn[0] = 1;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 1;
-    dOn[4] = 1;
-    dOn[5] = 1;
-    dOn[6] = 1;
+    LedDisplayArray[0] = 1;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 1;
+    LedDisplayArray[4] = 1;
+    LedDisplayArray[5] = 1;
+    LedDisplayArray[6] = 1;
     Serial.println("Testing the number ONE : 1");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_TWO_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 0;
-    dOn[2] = 1;
-    dOn[3] = 0;
-    dOn[4] = 0;
-    dOn[5] = 1;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 1;
+    LedDisplayArray[3] = 0;
+    LedDisplayArray[4] = 0;
+    LedDisplayArray[5] = 1;
+    LedDisplayArray[6] = 0;
     Serial.println("Testing the number TWO : 2");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_THREE_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 0;
-    dOn[4] = 1;
-    dOn[5] = 1;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 0;
+    LedDisplayArray[4] = 1;
+    LedDisplayArray[5] = 1;
+    LedDisplayArray[6] = 0;
     Serial.println("Testing the number THREE : 3");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_FOUR_TEST() {
-    dOn[0] = 1;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 1;
-    dOn[4] = 1;
-    dOn[5] = 0;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 1;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 1;
+    LedDisplayArray[4] = 1;
+    LedDisplayArray[5] = 0;
+    LedDisplayArray[6] = 0;
     Serial.println("Testing the number FOUR : 4");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_FIVE_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 1;
-    dOn[2] = 0;
-    dOn[3] = 0;
-    dOn[4] = 1;
-    dOn[5] = 0;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 1;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 0;
+    LedDisplayArray[4] = 1;
+    LedDisplayArray[5] = 0;
+    LedDisplayArray[6] = 0;
     Serial.println("Testing the number FIVE : 5");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_SIX_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 1;
-    dOn[2] = 0;
-    dOn[3] = 0;
-    dOn[4] = 0;
-    dOn[5] = 0;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 1;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 0;
+    LedDisplayArray[4] = 0;
+    LedDisplayArray[5] = 0;
+    LedDisplayArray[6] = 0;
     Serial.println("Testing the number SIX : 6");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_SEVEN_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 1;
-    dOn[4] = 1;
-    dOn[5] = 1;
-    dOn[6] = 1;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 1;
+    LedDisplayArray[4] = 1;
+    LedDisplayArray[5] = 1;
+    LedDisplayArray[6] = 1;
     Serial.println("Testing the number SEVEN : 7");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_EIGHT_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 0;
-    dOn[4] = 0;
-    dOn[5] = 0;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 0;
+    LedDisplayArray[4] = 0;
+    LedDisplayArray[5] = 0;
+    LedDisplayArray[6] = 0;
     Serial.println("Testing the number EIGHT : 8");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_Display_NINE_TEST() {
-    dOn[0] = 0;
-    dOn[1] = 0;
-    dOn[2] = 0;
-    dOn[3] = 1;
-    dOn[4] = 1;
-    dOn[5] = 0;
-    dOn[6] = 0;
+    LedDisplayArray[0] = 0;
+    LedDisplayArray[1] = 0;
+    LedDisplayArray[2] = 0;
+    LedDisplayArray[3] = 1;
+    LedDisplayArray[4] = 1;
+    LedDisplayArray[5] = 0;
+    LedDisplayArray[6] = 0;
     Serial.println("Testing the number NINE : 9");
     TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_ALL_ON_TEST() {
-  dOn[0] = 0;
-  dOn[1] = 0;
-  dOn[2] = 0;
-  dOn[3] = 0;
-  dOn[4] = 0;
-  dOn[5] = 0;
-  dOn[6] = 0;
+  LedDisplayArray[0] = 0;
+  LedDisplayArray[1] = 0;
+  LedDisplayArray[2] = 0;
+  LedDisplayArray[3] = 0;
+  LedDisplayArray[4] = 0;
+  LedDisplayArray[5] = 0;
+  LedDisplayArray[6] = 0;
   Serial.println("Testing the array being turned ON.");
   TEST_OUTPUT_DISPLAY_ARRAY();
 }
 
 void TEST_ALL_OFF_TEST() {
-  dOn[0] = 1;
-  dOn[1] = 1;
-  dOn[2] = 1;
-  dOn[3] = 1;
-  dOn[4] = 1;
-  dOn[5] = 1;
-  dOn[6] = 1;
+  LedDisplayArray[0] = 1;
+  LedDisplayArray[1] = 1;
+  LedDisplayArray[2] = 1;
+  LedDisplayArray[3] = 1;
+  LedDisplayArray[4] = 1;
+  LedDisplayArray[5] = 1;
+  LedDisplayArray[6] = 1;
   Serial.println("Testing the array being turned OFF.");
   TEST_OUTPUT_DISPLAY_ARRAY();
+}
+
+void Display_Off() {
+    for (int i = 0; i++; i < 7)
+    {
+        LedDisplayArray[i] = 0;
+    }
 }
